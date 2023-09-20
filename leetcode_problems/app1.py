bdde09ed6b45d65dffebe75c19a6ddb25eb531f6
User Name <jessnsq.codes@gmail.com>
2023-09-20 18:49:05 -0400

    left rotation - hackerrank

--- leetcode_problems/app1.py
+++ leetcode_problems/app1.py
@@ -58,38 +58,84 @@ Each of the  subsequent lines contains a query string, .
 
 #dynamic array that processes a series of queries, appending values to specific dynamic arrs based on type 1 queries and retrieving values from dynamic arrs for type 2 queries 
 
-def dynamicArray(n, queries):
-    #initialize lastAns to 0
-    lastAns = 0 
-    arr = list() #empty list
-    ans_arr = list() #empty list 
+# def dynamicArray(n, queries):
+#     #initialize lastAns to 0
+#     lastAns = 0 
+#     arr = list() #empty list
+#     ans_arr = list() #empty list 
     
-    #initializes the arr list by appending n empty lists to it, creating an array of empty lists, where each list represents a dynamic array.
-    for value in range(0, n):
-        arr.append(list())
+#     #initializes the arr list by appending n empty lists to it, creating an array of empty lists, where each list represents a dynamic array.
+#     for value in range(0, n):
+#         arr.append(list())
         
-    #loop through each query in the input queries 
-    for query in queries: 
-        query_type = query[0]
-        x = query[1]
-        y = query[2]
+#     #loop through each query in the input queries 
+#     for query in queries: 
+#         query_type = query[0]
+#         x = query[1]
+#         y = query[2]
         
-        #check the type of query, if it's 1 it means it's a type 1 query 
-        if query_type == 1: 
-            #calculate idx based on the formula provided, THis formula determines which dynamic arr we should append 'y' to
-            idx = (x^lastAns)%n
-            #appends y 
-            arr[idx].append(y)
+#         #check the type of query, if it's 1 it means it's a type 1 query 
+#         if query_type == 1: 
+#             #calculate idx based on the formula provided, THis formula determines which dynamic arr we should append 'y' to
+#             idx = (x^lastAns)%n
+#             #appends y 
+#             arr[idx].append(y)
             
-        #check if it's a type 2 query
-        elif query_type == 2: 
-            #calculate idx using the same formula 
-            idx = (x^lastAns)%n
-            #retrieve the value at the index 'y' from the dynamic arr 
-            lastAns = arr[idx][y%len(arr[idx])]
-            #append the retrieved value of lastAns to the ans list 
-            ans_arr.append(lastAns)
-    #return list containing all the results of all type 2 queries 
-    return ans_arr  
+#         #check if it's a type 2 query
+#         elif query_type == 2: 
+#             #calculate idx using the same formula 
+#             idx = (x^lastAns)%n
+#             #retrieve the value at the index 'y' from the dynamic arr 
+#             lastAns = arr[idx][y%len(arr[idx])]
+#             #append the retrieved value of lastAns to the ans list 
+#             ans_arr.append(lastAns)
+#     #return list containing all the results of all type 2 queries 
+#     return ans_arr  
 
+ 
+'''A left rotation operation on an array of size n  shifts each of the array's elements 1 unit to the left. Given an integer, d , rotate the array that many steps left and return the result. 
+
+Function Description
+
+Complete the rotateLeft function in the editor below.
+rotateLeft has the following parameters:
+int d: the amount to rotate by
+int arr[n]: the array to rotate
+Returns
+
+'''   
+
+#SIMPLE SOLUTION 
+
+def rotate_left(arr, d):
+    last = len(arr)-1
     
+    #iterates d times 
+    for i in range(0, d): 
+        print(i)
+        #assign the value of the first element to 'first'
+        first = arr[0]
+        
+        
+        for e in range(0, last):
+            #Inside the loop, each element at index e is replaced with the element at index e+1
+            arr[e] = arr[e+1]
+            print(arr[e])
+    
+    #After the second loop, the first element is assigned to the last position of the array
+        arr[last] = first
+    return arr 
+
+
+#OPTIMIZED SOLUTION
+def rotate_l(arr, d):
+    #slice notation 
+    rotated_arr = arr[d:] + arr[:d]
+    return rotated_arr
+    
+    
+arr = [1,2,3,4,5]
+arr1 = [1,2,3,4,5]
+d=2
+print(rotate_left(arr, d))
+print(rotate_l(arr1, d))
