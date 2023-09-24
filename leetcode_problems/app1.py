828ae2a1a638cdffa0369d0512df70360b0e4699
User Name <jessnsq.codes@gmail.com>
2023-09-23 22:32:19 -0400

    tower breakers"

--- leetcode_problems/app1.py
+++ leetcode_problems/app1.py
@@ -390,3 +390,18 @@ def findZigZagSequence(a, n):
     return
 
 
+'''TOWER BREAKERS'''
+'''Two players are playing a game of Tower Breakers! Player 1 always moves first, and both players always play optimally.The rules of the game are as follows:
+Initially there are n towers.
+Each tower is of height m.
+The players move in alternating turns.
+In each turn, a player can choose a tower of height  and reduce its height to y , where 1 is less or equal than y and Y is less than x; and y evenly divides x.
+If the current player is unable to make a move, they lose the game.
+Given the values of  n and m, determine which player will win. If the first player wins, return 1. Otherwise, return 2.'''
+
+def towerBreakers(n, m):
+    if n == 1 or m == 1 or n% 2 == 0:
+        return 1
+    else:
+        return 2 
+    
