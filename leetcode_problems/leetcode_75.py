389c11aed0325fb579add979a8c5176412a80633
User Name <jessnsq.codes@gmail.com>
2023-10-07 23:20:22 -0400

    1431.Kids with the greatest number of candies

--- leetcode_problems/leetcode_75.py
+++ leetcode_problems/leetcode_75.py
@@ -62,3 +62,30 @@ word1 = 'abc'
 word2 = 'pqrdf'
 print(len(word1)-1)
 print(merge_alternately(word1, word2))
+
+
+
+'''1431. Kids with the greates number of candies '''
+'''There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+Note that multiple kids can have the greatest number of candies.'''
+
+def kids_with_candies(candies, extra_candies):
+    #Find the max_num in the array
+    max_num_candies = max(candies)
+    #iterate through the list 
+    for num in range(len(candies)):
+        #check if current kid with candies + extra candies is greater or equal to max_num candies
+        if candies[num] + extra_candies >= max_num_candies:
+            #if yes, return True
+            candies[num] = True
+            #else False
+        else: 
+            candies[num] = False
+    #return list of candies(will return Boolean values)
+    return candies
+
+candies = [2,3,5,1,3]
+extra_candies = 3
+
+print(kids_with_candies(candies, extra_candies))
