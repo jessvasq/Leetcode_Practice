5dd692c5b70365d75e28da3a3690209234cbe940
User Name <jessnsq.codes@gmail.com>
2024-02-08 14:18:31 -0500

    Leetcode #122.Best time to buy and Sell stock II

--- leetcode_problems/leetcode_150.py
+++ leetcode_problems/leetcode_150.py
@@ -306,3 +306,25 @@ You must write an algorithm with O(log n) runtime complexity.'''
 
 # print(search_insert(nums, target))
 
+'''122. Best Time to Buy and Sell Stock II
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.'''
+
+prices = [7, 6, 4, 3, 1]  #Output=0
+
+def max_profit(prices):
+    #initialize the profit to 0
+    profit = 0 
+    #Iterate through the numbers and add the difference between the current number and the previous number to the profit
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            profit += prices[i] - prices[i-1]
+    
+    return profit 
+
+print(max_profit(prices))
+    
+
+
