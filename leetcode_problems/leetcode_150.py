48db952540546ccd82edabf9984109a4b700df50
User Name <jessnsq.codes@gmail.com>
2024-03-01 23:53:56 -0500

    Leetcode #104.Maximum Depth of Binary Tree

--- leetcode_problems/leetcode_150.py
+++ leetcode_problems/leetcode_150.py
@@ -442,3 +442,22 @@ def hasCycle(head):
         if slow == fast:
             return True
     return False
+
+'''104. Maximum Depth of Binary Tree
+Given the root of a binary tree, return its maximum depth. A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.'''
+
+# Check if the root is None
+# Traverse the left subtree and right subtree 
+# We can use recursion to traverse both subtrees 
+# add 1 at the end to account for the root level 
+
+def maxDepth(root):
+    if root is None: 
+        return 0 
+    
+    left_subtree = maxDepth(root.left)
+    right_subtree = maxDepth(root.right)
+    
+    return max(left_subtree, right_subtree) + 1
+
+    
